258da31926107c3edce07fdc743eca67b7f4d33c
Faith Magras <bdanielle092@gmail.com>
2020-09-23 12:06:43 -0500

    working on adding a comment

--- TabloidMVC/Repositories/CommentRepository.cs
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -3,13 +3,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TabloidMVC.Models;
 
 namespace TabloidMVC.Repositories
 {
-    public class CommentRepository
+    public class CommentRepository : BaseRepository, ICommentRepository
     { 
       
-        
-        
+       public CommentRepository(IConfiguration config) :base(config) { }
+        public void Add(Comment comment)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO COMMENT (Subject, Content)
+                         OUTPUT INSERTED.ID
+                          VALUES (@Subject, @Content)";
+                    cmd.Parameters.AddWithValue("@Subject", comment.Subject);
+                    cmd.Parameters.AddWithValue("@Content", comment.Content);
+
+                    comment.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
     }
 }
--- TabloidMVC/Repositories/ICommentRepository.cs
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TabloidMVC.Models;
 
 namespace TabloidMVC.Repositories
 {
     interface ICommentRepository
     {
+        void Add(Comment comment);
     }
 }
