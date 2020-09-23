376f81eeb1f6fe45880038d380bbab7b26d2be8e
Faith Magras <bdanielle092@gmail.com>
2020-09-23 11:47:19 -0500

    added view model for comments

---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository
+    { 
+      
+        
+        
+    }
+}
