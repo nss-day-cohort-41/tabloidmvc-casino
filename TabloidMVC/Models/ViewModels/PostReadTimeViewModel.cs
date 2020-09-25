0072ebc6e39bb8bef844d359cc5420307e8c8143
Rob Mixon <mixonroberth@gmail.com>
2020-09-25 14:43:53 -0500

    applied read time on post and added an if else statement for less than 1 minute

---
+++ TabloidMVC/Models/ViewModels/PostReadTimeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class PostReadTimeViewModel
+    {
+        public Post Post { get; set; }
+        public string ReadTime { get; set; }
+    }
+}
